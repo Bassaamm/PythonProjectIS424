dc9948ccaa03beb9256c46595ffd7a60055040ea
Bassaamm <bassamalqrni@gmail.com>
2023-11-16 02:02:12 +0300

    last

--- projectApp/models.py
+++ projectApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class user(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
 # Create your models here.
--- projectApp/views.py
+++ projectApp/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, 'projectApp/main.html')
+
+def login(request):
+   return render(request, 'projectApp/login.html')
